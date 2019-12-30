aa043e09e3d5d27c9fb1728dcb8ec3f9d858373d
ecp-alex <alex.mackenzie@englishcollege.cz>
2019-12-30 17:03:41 +0100

    STYLE R2 | NavBar | add react-icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,17 @@
 import React, { Component } from "react";
 import logo from "../assets/images/logo-classic-and-sportscar-centre.gif";
-import { FaAlignRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
-
+import { FaFacebook, FaYoutube, FaInstagram } from "react-icons/fa";
+import SiteData from "../assets/_data/_data";
 export default class Navbar extends Component {
-  state = {
-    isOpen: false
-  };
-
-  handleToggle = () => {
-    this.setState({ isOpen: !this.state.isOpen });
-  };
   render() {
     return (
-      <nav className="navbar">
+      <nav className="navbar" role="navigation">
         <div className="container">
           <div className="row">
             <div className="col-xs-12 col-sm-12 col-md-6 navbar-brand-wrap">
               <Link className="navbar-brand" to="/">
-                <img src={logo} alt="alt" />
+                <img src={logo} alt={SiteData.brand.name} />
               </Link>
               <button
                 type="button"
@@ -30,38 +23,29 @@ export default class Navbar extends Component {
               </button>
             </div>
             <div className="hidden-md-down col-md-6">
-              <h1>Selling classic cars worldwide for over 25 years</h1>
+              <h1>{SiteData.brand.strapline}</h1>
               <ul className="ul-header">
-                <li className="li-telephone">Telephone: 01944 758000</li>
+                <li className="li-telephone">
+                  Telephone: {SiteData.contact.telephone}
+                </li>
                 <li className="li-contact">
-                  <a href="http://www.classicandsportscar.ltd.uk/_wp190503/contact">
-                    Contact
-                  </a>
+                  <Link to={SiteData.contact.slug}>Contact</Link>
                 </li>
               </ul>
               <ul id="menu-social_menu" className="social_menu">
-                <li
-                  id="menu-item-4035"
-                  className="facebook menu-item menu-item-type-custom menu-item-object-custom menu-item-4035"
-                >
-                  <a href="https://www.facebook.com/classicandsportscarcentre/">
-                    facebook
+                <li className="facebook">
+                  <a href={SiteData.social.facebook}>
+                    <FaFacebook />
                   </a>
                 </li>
-                <li
-                  id="menu-item-4036"
-                  className="instagram menu-item menu-item-type-custom menu-item-object-custom menu-item-4036"
-                >
-                  <a href="https://www.instagram.com/classicandsportscar_centre/">
-                    instagram
+                <li className="instagram">
+                  <a href={SiteData.social.instagram}>
+                    <FaInstagram />
                   </a>
                 </li>
-                <li
-                  id="menu-item-4034"
-                  className="youtube menu-item menu-item-type-custom menu-item-object-custom menu-item-4034"
-                >
-                  <a href="https://www.youtube.com/channel/UCe47Y8zbsm1wJrb6H2LmKeA">
-                    youtube
+                <li className="youtube">
+                  <a href={SiteData.social.youtube}>
+                    <FaYoutube />
                   </a>
                 </li>
               </ul>
