e03cb7e12d650fe02ce0a9c57ad2b6a8d76a9c01
ecp-alex <alex.mackenzie@englishcollege.cz>
2020-01-02 16:12:37 +0100

    ENV | env.process.REACT_APP vars now used

--- src/assets/_data/_data-categories.js
+++ src/assets/_data/_data-categories.js
@@ -3,15 +3,15 @@ const catArray = {
     id: 2,
     title: "Classic Cars For Sale",
     slug: "/classic-cars-for-sale",
-    api: "http://localhost:3002/api/items/for-sale",
-    apiFeatured: "http://localhost:3002/api/items/for-sale/featured"
+    api: `${process.env.REACT_APP_API_ENDPOINT}items/for-sale`,
+    apiFeatured: `${process.env.REACT_APP_API_ENDPOINT}items/for-sale/featured`
   },
   Archive: {
     id: 2,
     title: "Classic Cars Sold",
     slug: "/classic-car-archive",
-    api: "http://localhost:3002/api/items/sold",
-    apiFeatured: "http://localhost:3002/api/items/sold/featured"
+    api: `${process.env.REACT_APP_API_ENDPOINT}items/sold`,
+    apiFeatured: `${process.env.REACT_APP_API_ENDPOINT}items/sold/featured`
   },
   Testimonials: {
     id: 3,
@@ -53,58 +53,6 @@ const catArray = {
     title: "XXX",
     slug: "/xxx"
   }
-  // 1: {
-  //   id: 2,
-  //   title: "Classic Cars For Sale",
-  //   slug: "/classic-cars-for-sale",
-  //   api: "http://localhost:3002/api/items/for-sale"
-  // },
-  // 2: {
-  //   category: 2,
-  //   title: "Classic Cars Sold",
-  //   slug: "/classic-car-archive",
-  //   api: "http://localhost:3002/api/items/sold"
-  // },
-  // 3: {
-  //   category: 3,
-  //   title: "CustomerTestimonials",
-  //   slug: "/testimonials"
-  // },
-  // 4: {
-  //   category: 4,
-  //   title: "Press / Media",
-  //   slug: "/press"
-  // },
-  // 5: {
-  //   category: 5,
-  //   title: "Latest News & Events",
-  //   slug: "/news"
-  // },
-  // 6: {
-  //   category: 6,
-  //   title: "Plates",
-  //   slug: "/plates"
-  // },
-  // 7: {
-  //   category: 7,
-  //   title: "Page Text",
-  //   slug: "/text"
-  // },
-  // 8: {
-  //   category: 8,
-  //   title: "XXX",
-  //   slug: "/xxx"
-  // },
-  // 9: {
-  //   category: 9,
-  //   title: "XXX",
-  //   slug: "/xxx"
-  // },
-  // 10: {
-  //   category: 10,
-  //   title: "XXX",
-  //   slug: "/xxx"
-  // }
 };
 
 export default catArray;
