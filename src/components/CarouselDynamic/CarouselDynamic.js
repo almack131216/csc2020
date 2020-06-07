05bf05f9c4fb3884306477b8a0dbcc6e85889a05
ecp-alex <alex.mackenzie@englishcollege.cz>
2020-06-07 11:41:28 +0200

    CarouselDynamic | now fetching data from itemId

--- src/components/CarouselDynamic/CarouselDynamic.js
+++ src/components/CarouselDynamic/CarouselDynamic.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { apiGetItemDetails } from "../../assets/js/Helpers";
+import Loading from "../Loading/Loading";
 
 const CarouselDynamic = props => {
+  const [loading, setLoading] = useState(true);
+  const [Images, setImages] = useState([]);
+
   var settings = {
     className: "slider carousel",
     dots: true,
@@ -13,20 +18,60 @@ const CarouselDynamic = props => {
     slidesToShow: 1,
     slidesToScroll: 1
   };
-  const Images = props.imgsArr;
-  console.log(Images);
-  const slides = Images.map((img, index) => {
-    //   let imgUrl = `http://localhost:8080/csc2020-img/images/carousel/${img.url}`;
-    let imgUrl = img.src;
 
+  useEffect(() => {
+    // if we receive itemId...
+    if(props.itemId){
+      setLoading(true);
+      // API - generate end point based on categoryName + itemId
+      const apiArr = {
+        categoryName: "Restoration",
+        itemId: props.itemId
+      };
+      const apiUrl = apiGetItemDetails(apiArr);
+      console.log('[CarouselDynamic] apiUrl: ', apiUrl);
+      // (END) API - generate
+
+      fetch(apiUrl)
+      .then(response => response.json())
+        .then(data => {
+          console.log("[CarouselDynamic] fetch() data: ", data);
+          let [itemPrimary, ...itemImageAttachments] = data;
+
+          const itemImages = [itemPrimary, ...itemImageAttachments];
+          console.log("[CarouselDynamic] fetch() itemImages: ", itemImages);
+
+          // put itemImages into array for carousel
+          const images = [];
+          for (let i = 0; i < itemImages.length; i++) {
+            images.push({
+              src: `${process.env.REACT_APP_IMG_DIR_LARGE}${itemImages[i].image}`,
+              name: itemImages[i].alt
+            });
+          }
+          setImages(images);
+          setLoading(false);
+        })
+        .catch(() => {
+          
+        });
+    } else {
+      // use imgsArr sent by parent
+      setImages(props.imgsArr);
+      console.log('[CarouselDynamic] Images: ', props.imgsArr);
+    }    
+  }, [props.itemId,props.imgsArr]);
+  
+  
+  const slides = Images ? Images.map((img, index) => {
     return (
       <div key={index}>
-        <img src={imgUrl} alt={img.alt} />
+        <img src={img.src} alt={img.alt} />
       </div>
     );
-  });
+  }) : null;
 
-  return <Slider {...settings}>{slides}</Slider>;
+  return !loading ? <Slider {...settings}>{slides}</Slider> : <Loading />;
 };
 
 export default CarouselDynamic;
