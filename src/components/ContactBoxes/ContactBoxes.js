abe3eebace33e719a87479c505de73b56eb5c6b8
ecp-alex <alex.mackenzie@englishcollege.cz>
2020-12-23 20:03:14 +0100

    TEXT | OpeningHours xmas switch

--- src/components/ContactBoxes/ContactBoxes.js
+++ src/components/ContactBoxes/ContactBoxes.js
@@ -1,11 +1,12 @@
 import React from "react";
 import SiteData from "../../assets/_data/_data";
 import SocialBtns from "..//SocialBtns/SocialBtns";
+import { getDateToday } from "../../assets/js/Helpers";
 import parse from "html-react-parser";
 
 const socialBtnsElement = <SocialBtns social={SiteData.social} classes="" />;
 
-const CB_OpeningHours = {
+const CB_OpeningHoursDefault = {
   title: "Office Hours",
   text: (
     <ul>
@@ -15,6 +16,28 @@ const CB_OpeningHours = {
     </ul>
   )
 };
+
+const CB_OpeningHoursChristmas = {
+  title: "Office Hours",
+  text: (
+    <ul>
+      <li>24th Dec. 8.30am-1pm</li>
+      <li>25th Dec. Closed</li>
+      <li>26th Dec. Closed</li>
+      <li>27th Dec. Closed </li>
+      <li>28th Dec. By appointment only </li>
+      <li>29th Dec. 8.30am-5.30pm</li>
+      <li>30th Dec. 8.30am-5.30pm</li>
+      <li>31st Dec. 8.30am-1pm</li>
+      <li>1st Jan. Closed </li>
+      <li>2nd Jan. Closed</li>
+      <li>3rd Jan. By appointment only </li>
+      <li>4th Jan. Re open as normal </li>
+    </ul>
+  )
+};
+
+const CB_OpeningHours = getDateToday() > '2020-12-22' && getDateToday() < '2021-01-04' ? CB_OpeningHoursChristmas : CB_OpeningHoursDefault;
 
 const CB_Address = {
   title: "Address",
