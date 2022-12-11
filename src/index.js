abeaad11b496ba32ceeb66706a11e267046d213e
ecp-alex <alex.mackenzie@englishcollege.cz>
2022-12-11 19:00:20 +0000

    CR | remove serviceWorker

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom";
 // import './index.css';
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 import { ItemProvider } from "./Context";
 import GA from './GoogleAnalytics'
 
@@ -23,4 +23,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
