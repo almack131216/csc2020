b467a934cfcae99f53608247d351003a271923cf
almack131216 <al_2003_1@hotmail.com>
2023-04-03 02:30:14 +0100

    RESP | big grid img aspect ratio for videos

--- src/components/Items/BigGrid/BigGrid.js
+++ src/components/Items/BigGrid/BigGrid.js
@@ -5,17 +5,19 @@ const BigGrid = props => {
   const items = [...props.items];
   // console.log("[BigGrid] item[0] = ", items);
 
-  // styling for Homepage News & Testimonials
+  // styling for Homepage News / Testimonials / Media / Videos
   let settingsDefault = {};
   settingsDefault.showExcerpt = true;
   settingsDefault.showCategoryLink = false;
   settingsDefault.layout = "item-card big";
-
   const settings = props.settings ? { ...props.settings } : settingsDefault;
+
+  let sectionClass = ["items-wrap","big-items-wrap"];
+  if (props.categoryName) sectionClass.push(props.categoryName);
   // (END) styling
 
   return (
-    <div className="items-wrap big-items-wrap">
+    <div className={sectionClass.join(" ")}>
       <div className="cards">
         {items
           ? items.map((item, index) => {
